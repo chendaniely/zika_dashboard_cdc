ff0416d6e6f451140472b7be0c2c9ce2adf941e7
Daniel Chen <Maveri2k@yahoo.com>
2016-03-22 18:15:35 -0400

    shiny dashboard template

---
+++ ui.R
@@ -0,0 +1,31 @@
+library(shinydashboard)
+
+dashboardPage(
+    dashboardHeader(title = "Basic dashboard"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+            menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "dashboard",
+                    fluidRow(
+                        box(plotOutput("plot1", height = 250)),
+
+                        box(
+                            title = "Controls",
+                            sliderInput("slider", "Number of observations:", 1, 100, 50)
+                        )
+                    )
+            ),
+
+            # Second tab content
+            tabItem(tabName = "widgets",
+                    h2("Widgets tab content")
+            )
+        )
+    )
+)
