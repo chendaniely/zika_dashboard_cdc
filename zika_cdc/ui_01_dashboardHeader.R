623304ae4a552d4676174c3978b37cc172cb245d
Daniel Chen <Maveri2k@yahoo.com>
2016-03-25 18:06:38 -0400

    GA code into body

--- zika_cdc/ui_01_dashboardHeader.R
+++ zika_cdc/ui_01_dashboardHeader.R
@@ -1,7 +1,6 @@
 library(shinydashboard)
 
 dashboard_header <- function(){
-    tags$head(includeScript("google_analytics.js"))
     dashboardHeader(
         title = "Basic dashboard",
         github_dropdown()
--- zika_cdc/ui_03_dashboardBody.R
+++ zika_cdc/ui_03_dashboardBody.R
@@ -2,6 +2,7 @@ library(shinydashboard)
 
 dashboard_body <- function(){
     dashboardBody(
+        tags$head(includeScript("google_analytics.js")),
         tabItems(
             main_dashboard_page(),
             colombia(),
