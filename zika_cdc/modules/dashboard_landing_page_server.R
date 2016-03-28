5f01a7982aa43e7e3f3b7294a59cef6047302931
Daniel Chen <Maveri2k@yahoo.com>
2016-03-27 23:14:18 -0400

    port over dash board landing page server module

---
+++ zika_cdc/modules/dashboard_landing_page_server.R
@@ -0,0 +1,69 @@
+dashboard_landing_page <- function(input, output, session){
+
+    output$basic_stat_nrow <- renderInfoBox({
+        valueBox(
+            nrow(combined_df),
+            "Rows of data", icon = icon("list"),
+            color = "aqua"
+        )
+    })
+
+    output$basic_stat_ndays <- renderInfoBox({
+        valueBox(
+            length(unique(combined_df$report_date)),
+            "Days of data", ,
+            icon = icon("calendar"),
+            color = "blue"
+        )
+    })
+
+    output$basic_stat_ncountry <- renderInfoBox({
+        valueBox(
+            combined_df %>%
+                tidyr::separate(col = location,
+                                into = c('country', 'location2', 'location3'),
+                                sep = '-',
+                                fill = 'right') %>%
+                dplyr::select(country) %>%
+                .$country %>%
+                dplyr::n_distinct() - 1,
+            "Countries",
+            icon = icon("globe"),
+            color = "light-blue"
+        )
+    })
+
+    output$data_version <- renderInfoBox({
+        valueBox(
+            substr(cdc_data_commit, 0, 7),
+            "Data Version", icon = icon("github"),
+            color = "aqua"
+        )
+    })
+
+    output$basic_stat_plot_ncases <- renderPlot({
+        country_cases <- combined_df %>%
+            tidyr::separate(col = location,
+                            into = c('country', 'location2', 'location3'),
+                            sep = '-',
+                            fill = 'right') %>%
+            dplyr::filter(value > 0) %>%
+            dplyr::count(country)
+        country_cases$country[is.na(country_cases$country)] <- "NA"
+
+        ggplot(data = country_cases,
+               mapping = aes(x = str_replace_all(country, '_', ' '),
+                             y = n,
+                             label = n)) +
+            geom_bar(stat = "identity", na.rm = FALSE) +
+            geom_text(aes(y = n + 0.05),
+                      position = position_dodge(0.9),
+                      vjust = 0,
+                      size = 5) +
+            scale_y_continuous(trans = "log10") +
+            labs(x = "Country",
+                 y = 'Number of case data > 0 (log 10)') +
+            theme_bw() +
+            theme(axis.text.x  = element_text(angle = 45, hjust = 1))
+    })
+}
--- zika_cdc/modules/dashboard_landing_page_ui.R
+++ zika_cdc/modules/dashboard_landing_page_ui.R
@@ -6,17 +6,17 @@ dashboard_landing_page_UI <- function(
     tabItem(tabName = "dashboard",
 
             fluidRow(
-                valueBoxOutput("basic_stat_nrow", width = 3),
-                valueBoxOutput("basic_stat_ndays", width = 3),
-                valueBoxOutput("basic_stat_ncountry", width = 3),
-                valueBoxOutput("data_version", width = 3)
+                valueBoxOutput(ns("basic_stat_nrow"), width = 3),
+                valueBoxOutput(ns("basic_stat_ndays"), width = 3),
+                valueBoxOutput(ns("basic_stat_ncountry"), width = 3),
+                valueBoxOutput(ns("data_version"), width = 3)
             ),
 
             fluidRow(
                 box(title = "Number of valid data points per country",
                     status = "primary",
                     solidHeader = TRUE,
-                    plotOutput('basic_stat_plot_ncases'),
+                    plotOutput(ns('basic_stat_plot_ncases')),
                     "Number of non-missing, and cases greater than 0 per country"
                 )
             )
